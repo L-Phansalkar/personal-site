37f4897397fe87cd26fb9e3100a8beb95bc996d1
LPM <11pm@LPMs-MacBook-Air.local>
2022-04-30 13:09:32 -0400

    form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -3,8 +3,20 @@ class ContactMe extends Component {
     render() {
         return (
             <div className="ContactMe">
-                <b>CONTACT ME:</b> 
+                <h3><b>CONTACT ME:</b></h3> 
+                <form method="post" name="contact_form"
+                action="contact-form-handler.php">
+            Your Name:
+         <input type="text" name="name"></input>
+
+            <br></br>Your Email Address:
+        <input type="text" name="email"></input>
+
+        <br></br>Your Message:
+     <textarea name="message"></textarea>
+    <input type="submit" value="Submit"></input>
+    </form>	
+    <h3><b>email: n.e.phansalkar@gmail.com</b></h3> 
             </div>
-            )}
-}
+)}}
 export default ContactMe
--- src/components/FeatJob.jsx
+++ src/components/FeatJob.jsx
@@ -19,14 +19,16 @@ class FeatJob extends Component {
                     <i>August 2016 - June 2017 </i>
                     <br></br>
                     Transferrable skills: 
-                    <br></br>Responsibilities:
-                    <br></br>Tutored and mentored students in core subjects and socio-emotional skills. 
-                    <br></br>Created and implemented data-driven individual and small group lesson plans. 
-                    <br></br>Managed six of my peers, and was responsible for the safety of 35 students. 
-                    <br></br>Developed and executed after school programming and provided homework help. 
-                    <br></br>Conducted parent phone calls and check-ins for absent students. 
-                    <br></br>Collaborated with a small, diverse group of City Year Philadelphia corps members across schools working to improve the City Year experience for all.
- 
+                    <br></br>
+                    Responsibilities:
+                    <div className= 'resp'>
+                    -> Tutored and mentored students in core subjects and socio-emotional skills. 
+                    <br></br>-> Created and implemented data-driven individual and small group lesson plans. 
+                    <br></br>-> Managed six of my peers, and was responsible for the safety of 35 students. 
+                    <br></br>-> Developed and executed after school programming and provided homework help. 
+                    <br></br>-> Conducted parent phone calls and check-ins for absent students. 
+                    <br></br>-> Collaborated with a small, diverse group of City Year Philadelphia corps members across schools working to improve the City Year experience for all.
+                    </div>
                 </p>
             </div>
             )}
