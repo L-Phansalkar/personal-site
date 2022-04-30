3afcf0df6f22df35cf47bea63e4a634b56b8db23
LPM <11pm@LPMs-MacBook-Air.local>
2022-04-30 12:25:29 -0400

    noelle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ class App extends Component {
         return (
             <div className="App">
             <header className="App-header">
-            <h1 className="App-title">L PHANSALKAR</h1>
+            <h1 className="App-title">NOELLE PHANSALKAR</h1>
             <h2 className="App-title">fullstack software engineer</h2>
               <CodingLangs></CodingLangs>
             </header>
