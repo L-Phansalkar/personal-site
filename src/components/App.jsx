c7d9ca8ce7e636a09cd37ea059a82b4cc288d2cf
LPM <11pm@LPMs-MacBook-Air.local>
2022-05-14 22:17:51 -0400

    hide with slider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,26 +9,40 @@ import ContactMe from "./ContactMe";
 
 class App extends Component {
     render() {
+      function valCheck() {
+        var remember = document.getElementById("slider");
+        var borapp = document.getElementById("borapp")
+        if (remember.checked) {
+          console.log("checked");
+          borapp.classList.add('hidden');
+        } else {
+          console.log("You didn't check it! Let me check it for you.");
+          borapp.classList.remove('hidden');
+        }
+      }
         return (
-            <div className="App">
+            <div className="App" >
             <header className="App-header">
               <label class="switch">
-              <input type="checkbox" id="slider"></input>
+              <input type="checkbox" id="slider" onClick = {valCheck}></input>
               <span class="slider round"></span>
             </label>
+            
             <h1 className="App-title">NOELLE PHANSALKAR</h1>
             <h2 className="App-title">fullstack software engineer</h2>
               <CodingLangs></CodingLangs>
             </header>
-            <div className="MainBody">
+            <div className="MainBody" id ="borapp">
             <img src={logo} className="App-logo" alt="logo" />
              <AboutMe></AboutMe>
              <FeatProj></FeatProj>
             <ContactMe></ContactMe>
             <FeatJob></FeatJob>
              <FactsNFigures></FactsNFigures>
+             
             <p className="made">made with: React | HTML | JSX | CSS </p>
             </div>
+            
           </div>
         );
     }
