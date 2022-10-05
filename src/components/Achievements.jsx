a0934db162843ee9f7de5644c3555bceeaa0ec42
LPM <11pm@LPMs-MacBook-Air.local>
2022-10-05 14:13:03 -0400

    outer places addition

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -4,10 +4,16 @@ class Achievements extends Component {
     render() {
         return (
             <div className="App">
-            <h2>ACHIEVEMENTS</h2>
-            of which I am particularly proud
-            <h4></h4>
-            <Canvas></Canvas>
+            <h2>PUBLICATIONS</h2>
+            <a href="http://apadivision16.org/wp-content/uploads/2021/03/TSP-Spring-2021.pdf#page=21" target="_blank">
+              <h4>How To Be Anti-Racist: A School Psychology Starter Pack</h4>
+            </a>
+            <a href="https://outerplaces.tumblr.com/" target="_blank">
+              <h4>Outer Places Social Media Page</h4>
+            </a>
+            <h5>Outer Places is no longer online, but you can check out this  <a href="https://web.archive.org/web/20150630183736/http://www.outerplaces.com/tag?tag=Technology" target="_blank">
+              web archive
+            </a> to see some of the technology articles I wrote in 2015</h5>
           </div>
             )}
 }
